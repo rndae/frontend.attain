fa2a0a66ed5b12c07e75fb3a28f52c112b6d7de1
Isaac Copenhaver <isaaccopenhaver@gmail.com>
2024-06-12 10:56:30 -0400

    fixed volume labels

--- public/scripts/loadChart.js
+++ public/scripts/loadChart.js
@@ -63,14 +63,14 @@ function drawVolumeChart(type, data) {
         labels: labels,
         datasets: [
             {
-                label: 'Up Volumes',
+                label: 'Upstream Volume',
                 data: upVol,
                 backgroundColor: 'rgba(75, 192, 192, 0.2)',
                 borderColor: 'rgba(75, 192, 192, 1)',
                 borderWidth: 1
             },
             {
-                label: 'Down Volumes',
+                label: 'Downstrea Volume',
                 data: downVol,
                 backgroundColor: 'rgba(255, 99, 132, 0.2)',
                 borderColor: 'rgba(255, 99, 132, 1)',
