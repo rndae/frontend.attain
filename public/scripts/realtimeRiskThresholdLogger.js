25404e7f384a8ba3ecb6f822fffc189baef2b1af
rndae <rndae@users.noreply.github.com>
2024-08-29 01:56:29 -0400

    secondary risk info segment added. updated primary risks to new data format from backend

--- public/scripts/realtimeRiskThresholdLogger.js
+++ public/scripts/realtimeRiskThresholdLogger.js
@@ -4,11 +4,17 @@ const divergeType = "Diverge";
 const expressType = "Express";
 const weavingType = "Weaving";
 const rampType = "Ramp";
+const secondaryType = "secondary";
 
 const timeIntervalInMS = 60000;
 
 async function valuesAboveThresholdFromArray(riskType, predictionArray) {
-  const filteredArray = predictionArray.filter(item => item.rank.includes(riskType));
+  const filteredArray = predictionArray.filter(item => item.rank != undefined && item.rank.includes(riskType) && item.type.includes("primary"));
+  return filteredArray.length;
+}
+
+async function secondaryValues(secondaryType, predictionArray) {
+  const filteredArray = predictionArray.filter(item => item.type != undefined && item.type.includes(secondaryType));
   return filteredArray.length;
 }
 
@@ -21,6 +27,7 @@ async function updatePrimaryRiskInformation() {
   const expressAboveThreshold = await valuesAboveThresholdFromArray(expressType, riskDataArray);
   const weavingAboveThreshold = await valuesAboveThresholdFromArray(weavingType, riskDataArray);
   const rampAboveThreshold = await valuesAboveThresholdFromArray(rampType, riskDataArray);
+  const secondarySegments = await secondaryValues(secondaryType, riskDataArray);
 
   document.getElementById('basic-realtime-info').textContent = basicAboveThreshold;
   document.getElementById('merge-realtime-info').textContent = mergeAboveThreshold;
@@ -28,15 +35,16 @@ async function updatePrimaryRiskInformation() {
   document.getElementById('express-realtime-info').textContent = expressAboveThreshold;
   document.getElementById('weaving-realtime-info').textContent = weavingAboveThreshold;
   document.getElementById('ramp-realtime-info').textContent = rampAboveThreshold;
+  document.getElementById('secondary-realtime-info').textContent = secondarySegments;
   populateSegmentDropdowns(riskDataArray);
 }
 
 function populateSegmentDropdowns(riskDataArray) {
-  const segmentTypes = [basicType, mergeType, divergeType, expressType, weavingType, rampType];
+  const segmentTypes = [basicType, mergeType, divergeType, expressType, weavingType, rampType, secondaryType];
   segmentTypes.forEach(type => {
     const dropdownMenu = document.getElementById(`${type.toLowerCase()}-segments-list`);
     dropdownMenu.innerHTML = ''; 
-    const filteredArray = riskDataArray.filter(item => item.rank.includes(type));
+    const filteredArray = riskDataArray.filter(item => item.rank != undefined && item.rank.includes(type) || (item.type != undefined && item.type.includes(type)));
     filteredArray.forEach(segment => {
       const segmentId = segment.segment_id;
       const dropdownItem = document.createElement('a');
