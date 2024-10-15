a7b4f03f24ba60bb5e64bcf4cfdbb75001bcb56b
Zubayer Islam <zubayer87@gmail.com>
2024-10-15 15:04:45 -0400

    ZB | added fixedUpdateCharts

--- public/scripts/updateChart.js
+++ public/scripts/updateChart.js
@@ -1,7 +1,7 @@
 function updateCharts(chartId, newData) {
     newData = newData.slice(10, 15);
     if (chartId === "speedChart") {
-        speedChart.data.datasets[0].data = newData.map(item => item.speed);
+        speedChart.data.datasets[0].data = newData.map(item => item.speed > 0 ? item.speed : 0);
         speedChart.data.labels = newData.map(item => item.time);
         speedChart.update();
     } else if (chartId === "riskChart") {
@@ -10,14 +10,13 @@ function updateCharts(chartId, newData) {
         riskChart.update();
     } else if (chartId === "volumeChart") {
         console.log(newData);
-        volumeChart.data.datasets[0].data = newData.map(item => item.upstream_volume);
-        volumeChart.data.datasets[1].data = newData.map(item => item.volume);
-        volumeChart.data.datasets[2].data = newData.map(item => item.downstream_volume);
-
+        volumeChart.data.datasets[0].data = newData.map(item => item.upstream_volume > 0 ? item.upstream_volume : 0);
+        volumeChart.data.datasets[1].data = newData.map(item => item.volume > 0 ? item.volume : 0);
+        volumeChart.data.datasets[2].data = newData.map(item => item.downstream_volume > 0 ? item.downstream_volume : 0);
         volumeChart.data.labels = newData.map(item => item.time);
         volumeChart.update();
     } else if (chartId === "stdChart") {
-        stdChart.data.datasets[0].data = newData.map(item => item.speed_std);
+        stdChart.data.datasets[0].data = newData.map(item => item.speed_std > 0 ? item.speed_std : 0);
         stdChart.data.labels = newData.map(item => item.time);
         stdChart.update();
     }
